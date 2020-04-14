996a442a0ca0f520a2b35e902bf270ad3b931f1e
Vito Botta <vito@botta.me>
2020-04-15 00:01:39 +0300

    Add Hetzner Cloud Controller Manager

---
+++ hetzner-cloud-controller-manager/HetznerCloudControllerManager.ts
@@ -0,0 +1,52 @@
+import * as k8s from '@pulumi/kubernetes'
+import * as pulumi from '@pulumi/pulumi'
+
+export interface HetznerCloudControllerManagerArgs {
+  apiToken?: pulumi.Input<string>,
+  version?: pulumi.Input<string>,
+}
+
+export class HetznerCloudControllerManager extends pulumi.ComponentResource  {
+  constructor(
+    appName: string,
+    args: HetznerCloudControllerManagerArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    super(appName, appName, {}, opts)
+
+    const config: pulumi.Config = new pulumi.Config(appName)
+
+    const apiToken = pulumi.output(args.apiToken || config.getSecret('apiToken') || "blah")
+    const version = args.version || "v1.5.1"
+
+    const manifestURL = `https://raw.githubusercontent.com/hetznercloud/hcloud-cloud-controller-manager/master/deploy/${version}.yaml`;
+
+    const secret = new k8s.core.v1.Secret(`${appName}-hetzner-cloud-token`, 
+      {
+        metadata: {
+          name: "hcloud",
+          namespace: "kube-system",
+        },
+        stringData: {
+          token: apiToken
+        },
+      },
+      {
+        parent: this,
+      },      
+    )    
+
+    const manifest = new k8s.yaml.ConfigFile(
+      `${appName}-manifest`,
+      {
+        file: manifestURL,
+      },
+      {
+        parent: this,
+        dependsOn: [
+          secret,
+        ],
+      },
+    )       
+  }
+}
