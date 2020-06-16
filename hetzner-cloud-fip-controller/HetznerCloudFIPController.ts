8b548593ee8e00de286ee8583cacd9e0cfb4b8c7
Vito Botta <vito@botta.me>
2020-06-16 16:00:09 +0300

    Update floating IP controller

--- hetzner-cloud-fip-controller/HetznerCloudFIPController.ts
+++ hetzner-cloud-fip-controller/HetznerCloudFIPController.ts
@@ -1,9 +1,5 @@
 import * as k8s from '@pulumi/kubernetes'
 import * as pulumi from '@pulumi/pulumi'
-import * as fs from "fs";
-import * as path from "path";
-import * as nodepath from "path";
-import * as shell from "shelljs";
 
 declare var require: any;
 
@@ -12,6 +8,7 @@ export interface HetznerCloudFIPControllerArgs {
   apiToken?: pulumi.Input<string>,
   namespace?: pulumi.Input<string>,
   nodeAddressType?: pulumi.Input<string>,
+  replicaCount?: pulumi.Input<number>,
 }
 
 export class HetznerCloudFIPController extends pulumi.ComponentResource  {
@@ -28,6 +25,7 @@ export class HetznerCloudFIPController extends pulumi.ComponentResource  {
     const namespace = args.namespace || config.get("namespace") || "hcloud-fip-controller"
     const addresses = args.addresses
     const nodeAddressType = args.nodeAddressType || "internal"
+    const replicaCount = args.replicaCount || 3
 
     const ns = new k8s.core.v1.Namespace(
       `${appName}-ns`,
@@ -39,24 +37,24 @@ export class HetznerCloudFIPController extends pulumi.ComponentResource  {
       { parent: this },
     )        
 
-    const repoDir = path.resolve(`/tmp/hetzner-cloud-fip-controller`);
 
-    if (!fs.existsSync(nodepath.join(repoDir))) {
-      shell.exec(`git clone https://github.com/vitobotta/helm-charts.git ${repoDir}`)
-    }    
+    const token: pulumi.Output<string> = pulumi.interpolate `HCLOUD_API_TOKEN: '${apiToken}'`
 
     const chart = new k8s.helm.v3.Chart(
       appName,
       {
-        path: nodepath.join(repoDir, "charts", "hcloud-fip-controller"),
+        chart: "hcloud-fip-controller",
+        fetchOpts: {
+          repo: 'https://cbeneke.github.com/helm-charts',
+        },        
         namespace: namespace,
         values: {
-          deploymentKind: "DaemonSet",
+          replicaCount: replicaCount,
           configInline: {
             hcloud_floating_ips: addresses,
             node_address_type: nodeAddressType
           },
-          hcloudAPIToken: apiToken
+          secretInline: token
         }
       },
       {
@@ -65,6 +63,6 @@ export class HetznerCloudFIPController extends pulumi.ComponentResource  {
           ns,
         ],
       },
-    )   
+    )       
   }
 }
