e3b087527dd2deff33f12917fa3eea442cc0470b
Vito Botta <vito@botta.me>
2020-07-20 02:11:26 +0300

    Update Velero

--- velero/config.ts
+++ velero/config.ts
@@ -2,7 +2,7 @@ import * as pulumi from '@pulumi/pulumi'
 
 const config: pulumi.Config = new pulumi.Config('velero')
 
-export const imageTag: string = config.get('imageTag') || "v1.4.0"
+export const imageTag: string = config.get('imageTag') || "v1.4.2"
 export const chartVersion: string = config.get('chartVersion') || "2.12.0"
 export const namespace: string = config.get('namespace') || "velero"
 export const s3Bucket: string = config.require('s3Bucket')
