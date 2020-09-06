7825e1ed9d5c5d171ca48ebe3119e3800b5820b1
Vito Botta <vito@botta.me>
2020-09-06 19:03:34 +0300

    Upgrade cert-manager

--- cert-manager/config.ts
+++ cert-manager/config.ts
@@ -2,7 +2,7 @@ import * as pulumi from '@pulumi/pulumi'
 
 const config: pulumi.Config = new pulumi.Config('cert-manager')
 
-export const version: string = config.get('version') || "v0.15.2"
+export const version: string = config.get('version') || "v1.0.1"
 export const namespace: string = config.get('namespace') || "cert-manager"
 export const email: string = config.require('email')
 export const cloudflareEmail: string = config.require('cloudflareEmail')
